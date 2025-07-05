2c9e211b220ef77edd2573112ca4052484a7514c
RiteshBorse <riteshborse008@gmail.com>
2025-07-05 21:26:34 +0530

    Empty Functions created

--- backend/routes/events.routes.js
+++ backend/routes/events.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { getEventById, getEvents, getEventSeatsAndTimings, postEvent } from "../controllers/events.controller.js";
+import { deleteMyEvent, getBookings, getEventById, getEvents, getEventSeatsAndTimings, getMyBookings, getMyEventById, getMyEvents, postEvent, updateMyEvent } from "../controllers/events.controller.js";
 
 const router = Router();
 router.get('/get-events' , authenticate , getEvents);
 router.get('/get-events/:id' , authenticate , getEventById);
 router.post('/add-events' , authenticate , postEvent);
 router.get('/get-seats-times/:id' , authenticate , getEventSeatsAndTimings);
+router.get('/get-my-bookings' , authenticate , getMyBookings)
+
+
+router.get('/get-my-events' , authenticate , getMyEvents);
+router.get('/get-my-events/:id' , authenticate , getMyEventById);
+router.post('/update-my-event' , authenticate , updateMyEvent);
+router.delete('/delete-my-event/:id' , authenticate , deleteMyEvent);
+router.get('/get-bookings' , authenticate , getBookings);
 export default router;

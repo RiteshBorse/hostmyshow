4d27871728e5c7ce01f68768f008b33edace3cda
RiteshBorse <riteshborse008@gmail.com>
2025-07-05 16:09:03 +0530

    Add Event Function completed

--- backend/routes/events.routes.js
+++ backend/routes/events.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { getEventById, getEvents } from "../controllers/events.controller.js";
+import { getEventById, getEvents, postEvent } from "../controllers/events.controller.js";
 
 const router = Router();
 router.get('/get-events' , authenticate , getEvents);
 router.get('/get-events/:id' , authenticate , getEventById);
+router.post('/add-events' , authenticate , postEvent);
 export default router;

3a13ac6a2c317618014ce7067874061413c864d0
RiteshBorse <riteshborse008@gmail.com>
2025-07-05 21:00:11 +0530

    Get Event time and show

--- backend/routes/events.routes.js
+++ backend/routes/events.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { getEventById, getEvents, postEvent } from "../controllers/events.controller.js";
+import { getEventById, getEvents, getEventSeatsAndTimings, postEvent } from "../controllers/events.controller.js";
 
 const router = Router();
 router.get('/get-events' , authenticate , getEvents);
 router.get('/get-events/:id' , authenticate , getEventById);
 router.post('/add-events' , authenticate , postEvent);
+router.get('/get-seats-times/:id' , authenticate , getEventSeatsAndTimings);
 export default router;

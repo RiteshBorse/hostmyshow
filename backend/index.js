3a732775066390bb6a118be4c85078ea10c8c0dd
RiteshBorse <riteshborse008@gmail.com>
2025-07-06 01:54:28 +0530

    Payment Gateway integrated ...

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,29 @@ import dotenv from "dotenv"
 import cors from "cors"
 import { connectDB } from "./utils/connectDB.js";
 import cookieParser from "cookie-parser";
+dotenv.configDotenv();
 const app = express();
+
+import Razorpay from "razorpay"
+export const razorpayInstance = new Razorpay({
+    key_id : process.env.RAZORPAY_KEY,
+    key_secret : process.env.RAZORPAY_KEY_SECRET
+})
 app.use(express.json());
 app.use(cors({
     origin : "http://localhost:5173",
     credentials : true
 }));
 app.use(cookieParser());
-dotenv.configDotenv();
 
 import userRouter from "./routes/user.routes.js"
 app.use("/api/user" , userRouter);
 
 import eventsRouter from "./routes/events.routes.js"
 app.use("/api/events" , eventsRouter);
+
+import paymentRouter from "./routes/payment.routes.js"
+app.use("/api/payment" , paymentRouter)
 
 app.listen(process.env.PORT || 8000 , ()=> {
     connectDB();
